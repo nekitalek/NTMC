cd07def847de98c940e020540a290c9d1019ea38
Nikita Dedov <nekitness@gmail.com>
2023-06-30 21:05:37 +0300

    Add files via upload

---
+++ Лаб1/main (1).py
@@ -0,0 +1,147 @@
+from timeit import default_timer as timer
+def extended_euclidean_algorithm(a, b):#Расширенный алгоритм Евклида
+    if a < b:
+        a, b = b, a
+    Ichar, Rchar, Xchar, Ychar, Qchar = 'i', 'r', 'x', 'y', 'q'
+    xi_1 = 1
+    xi = 0
+    yi_1 = 0
+    yi = 1
+    ri_1 = a
+    ri = b
+    i = 0
+    temp_r, temp_x, temp_y = 1, 1, 1
+    print("========================================================================\n\tРасширенный алгоритм Евклида")
+    print("% 3c" % Ichar, "% 25c" % Rchar, "% 25c" % Xchar, "% 25c" % Ychar, "% 25c" % Qchar)
+    print("% 3d" % i, "% 25d" % ri_1, "% 25d" % xi, "% 25d" % yi)
+    while temp_r!=0:
+        i+= 1
+        q = int(ri_1/ri)
+        temp_r = ri_1 % ri
+        print("% 3d" %i,"% 25d" %ri_1, "% 25d" %xi, "% 25d" %yi, "% 25d" %q)
+        if temp_r == 0:
+            break
+        temp_x = xi_1 - q*xi
+        temp_y = yi_1 - q*yi
+        xi_1=xi
+        yi_1=yi
+        xi=temp_x
+        yi=temp_y
+        ri_1 = ri
+        ri = temp_r
+    print("НОД = ", ri)
+    print("Первый коэфф: ", xi)
+    print("Второй коэфф: ", yi)
+    return
+
+def extended_binary_euclidean_algorithm(a, b):
+    Ichar, Uchar, Vchar, Achar, Bchar, Cchar, Dchar  = 'i', 'u', 'v', 'A', 'B', 'C', 'D'
+    g = 1
+    while a % 2 == 0 and b % 2 == 0:
+        a = a // 2
+        b = b // 2
+        g = g * 2
+    u = a
+    v = b
+    A = 1
+    B = 0
+    C = 0
+    D = 1
+    i = 0
+    print("========================================================================\n\tРасширенный бинарный алгоритм Евклида")
+    print("% 3c" % Ichar, "% 42c" %Uchar, "% 42c" %Vchar, "%42c" % Achar, "% 42c" %Bchar, "% 42c" %Cchar, "% 42c" %Dchar)
+    while u!=0:
+        print("% 3d" %i,"% 42d" %u, "% 42d" %v, "% 42d" %A, "% 42d" %B, "% 42d" %C, "% 42d" %D)
+        i += 1
+        while u % 2 == 0:
+            u = u // 2
+            if A % 2 == 0 and B % 2 == 0:
+                A = A // 2
+                B = B // 2
+            else:
+                A = (A + b) // 2
+                B = (B - a) // 2
+        while v % 2 == 0:
+            v = v // 2
+            if C % 2 == 0 and D % 2 == 0:
+                C = C // 2
+                D = D // 2
+            else:
+                C = (C + b) // 2
+                D = (D - a) // 2
+        if u >= v:
+            u = u - v
+            A = A - C
+            B = B - D
+        else:
+            v = v - u
+            C = C - A
+            D = D - B
+    d = g * v
+    x = C
+    y = D
+    print("НОД = ", int(d))
+    print("Первый коэфф = ", int(x))
+    print("Второй коэфф = ", int(y))
+    return
+
+def extended_euclidean_algorithm_with_us(a, b):
+    if a < b:
+        a, b = b, a
+    Ichar, Rchar, Xchar, Ychar, Qchar = 'i', 'r', 'x', 'y', 'q'
+    xi_1 = 1
+    xi = 0
+    yi_1 = 0
+    yi = 1
+    ri_1 = a
+    ri = b
+    i = 0
+    temp_r, temp_x, temp_y = 1, 1, 1
+    print("========================================================================\n\tРасширенный бинарный алгоритм Евклида с усечёнными остатками")
+    print("% 3c" % Ichar, "% 85c" % Rchar, "% 42c" % Xchar, "% 42c" % Ychar, "% 10c" % Qchar)
+    print("% 3d" % i, "% 85d" % ri_1, "% 42d" % xi, "% 42d" % yi)
+    while temp_r!=0:
+        i+= 1
+        q = int(ri_1/ri)
+        temp_r = ri_1 % ri
+        print("% 3d" %i,"% 85d" %ri_1, "% 42d" %xi, "% 42d" %yi, "% 10d" %q)
+        if temp_r == 0:
+            break
+        temp_x = xi_1 - q*xi
+        temp_y = yi_1 - q*yi
+        xi_1=xi
+        yi_1=yi
+        xi=temp_x
+        yi=temp_y
+        if temp_r >= (ri/2):
+            temp_r = ri - temp_r
+            xi = xi_1 - xi
+            yi = yi_1 - yi
+        ri_1 = ri
+        ri = temp_r
+    print("НОД = ", ri)
+    print("Первый коэфф: ", xi)
+    print("Второй коэфф: ", yi)
+    return
+
+A1 = 10280584362558385364313785041802045979548186541286500517257326693113600668924649
+B1 = 14598422008191898215363941644699891185829894490005557691091371425993340283543011
+A2 = 10280584362558385364313785041802045979548186541286500517257326693113600668924649
+B2 = 14598422008191898215363941644699891185829894490005557691091371425993340283543011
+A3 = 10280584362558385364313785041802045979548186541286500517257326693113600668924649
+B3 = 14598422008191898215363941644699891185829894490005557691091371425993340283543011
+#A1 = 3857119352587748339
+#B1 = 7725867209565428117
+#A2 = 1508570634299956106041083924384400803049
+#B2 = 809549785931263573750937056404595161133
+#A3 = 10280584362558385364313785041802045979548186541286500517257326693113600668924649
+#B3 = 14598422008191898215363941644699891185829894490005557691091371425993340283543011
+start = timer()
+extended_euclidean_algorithm(A1, B1)
+print("Время: {:g} secs".format(timer() - start))
+start = timer()
+extended_binary_euclidean_algorithm(A2, B2)
+print("Время: {:g} secs".format(timer() - start))
+start = timer()
+extended_euclidean_algorithm_with_us(A3, B3)
+print("Время: {:g} secs".format(timer() - start))
---
+++ Лаб4/ConsoleApp1/miller_rabin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Diagnostics;
+using System.IO;
+
+namespace miller_rabin
+{
+    class Program_MillerRabin
+    {
+        public static BigInteger RandomBigInteger(BigInteger n, Random rnd)
+        {
+            byte[] bytes = n.ToByteArray();
+            BigInteger a;
+            do
+            {
+                rnd.NextBytes(bytes);
+                a = new BigInteger(bytes);
+            } while (a > n - 2 || a < 2);
+
+            return a;
+        }
+
+        public static bool is_prime(BigInteger n)
+        {
+
+            BigInteger a, r, y;
+            int i = 0, s = 0, j = 0;
+            
+            bool isprime = true;
+            
+            Random rnd = new Random();
+
+            
+
+            while (i < 100)
+            {
+                r = n - 1;
+                do
+                {
+                    r = r / 2;
+                    s++;
+
+                } while (r % 2 == 0);
+                //a = RandomBigInteger(n, rnd);
+                a = rnd.Next(1, (int)n-2);
+                y = BigInteger.ModPow(a, r, n);
+                
+                if (y != 1 && y != n - 1)
+                {
+                    j = 1;
+                    while (j <= s - 1 && y != n - 1)
+                    {
+                        y = BigInteger.ModPow(y, 2, n);
+                        
+                        if (y == 1)
+                        {
+                            isprime = false;
+                            break;
+                        }
+                        j++;
+                    }
+                   
+                    if (y != n - 1)
+                    {
+                        isprime = false;
+                        break;
+                    }
+                }
+                
+                i++;
+            }
+            return isprime;
+        }
+    }
+}
